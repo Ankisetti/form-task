d2dc64924f783e256004f8d88edaafc5c618420c
Ankisetti <supraja705@gmail.com>
2023-10-04 12:53:13 -0700

    react tasks

---
+++ form-task/src/App.js
@@ -0,0 +1,12 @@
+
+import FormControlled from './FormControlled';
+
+function App() {
+  return (
+    <div>
+      <FormControlled/>
+    </div>
+  );
+}
+
+export default App;
---
+++ form-task/src/FormControlled.js
@@ -0,0 +1,16 @@
+import React from 'react' 
+
+
+
+  const FormControlled =()=>{
+    return(
+        <>
+        <form>
+        <label>Username</label>
+        <input type={'text'}placeholder={'enter username'}/>
+        </form>
+        </>
+
+    )
+}
+export default FormControlled;
